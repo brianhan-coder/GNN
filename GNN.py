521f88ee1a1909f4c239a7d4cd9b2cb900b066f9
Gaoyuan Wang <gaoyuanwang@Gaoyuans-MacBook-Pro.local>
2022-08-08 12:50:25 -0400

    added PDB to graph library

--- GNN.py
+++ GNN.py
@@ -1,12 +1,27 @@
 import numpy as np
-
+import torch
 import feature_embedding
+import PDB2Graph
+#import os
+#os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
+### loading features
 featureData = feature_embedding.FeatureData()
 featureData.readFeatureFile("AA_features/AA_charge.dat","charge")
 featureData.readFeatureFile("AA_features/AA_hydrophobic.dat","hydrophobicity")
 
+# loading PDB for the protein structure
+nodes=PDB2Graph.nodes('2VIU')
+edges=PDB2Graph.edges('2VIU')
+print(nodes)
 
+
+### readin feature list of all amino acids
+complete_list_feature=[]
 for aminoAcid in featureData.buildProtein('F','A','B'):
-    print(aminoAcid.charge)
-    
+    my_features=[float(tmp) for tmp in list(aminoAcid)]
+    complete_list_feature.append(my_features)
+
+#nodes_feature=torch.tensor(complete_list_feature,dtype=torch.float) # feature vector of all nodes
+
+#edge_index = torch.tensor([[0, 1, 2, 0, 3],[1, 0, 1, 3, 2]], dtype=torch.long) # edges, 1st list: index of the source nodes, 2nd list: index of target nodes.
---
+++ PDB2Graph.py
@@ -0,0 +1,68 @@
+import numpy as np
+from proteingraph import read_pdb
+from proteingraph.pin import pdb2df,compute_chain_pos_aa_mapping
+from IPython.display import HTML
+import pypdb 
+from biopandas.pdb import PandasPdb
+## proteingraph version "0.3.0"
+
+
+#pdb_file = pypdb.get_pdb_file('2viu', filetype='cif', compression=False)
+
+
+def number_of_nodes(pdb_id):
+    G = read_pdb('pdb_library/'+str(pdb_id)+'.pdb')
+    return G.number_of_nodes()
+
+def number_of_edges(pdb_id):
+    G = read_pdb('pdb_library/'+str(pdb_id)+'.pdb')
+    return G.number_of_edges()
+
+def edges(pdb_id):
+    path='pdb_library/'+str(pdb_id)+'.pdb'
+    G = read_pdb(path)
+    res2node=residueID2nodeID(pdb_id)
+
+    start_aa=[]
+    target_aa=[]
+
+
+    for edge in G.edges():
+        residue_start=str(edge[0][:-3])
+        residue_target=str(edge[1][:-3])
+        node_start=res2node[residue_start]
+        node_target=res2node[residue_target]
+        start_aa.append(node_start)
+        target_aa.append(node_target)
+
+    result_edges=[start_aa,target_aa]
+    return result_edges
+
+def residueID2nodeID(pdb_id):
+    path='pdb_library/'+str(pdb_id)+'.pdb'
+    pdb_df=pdb2df(path)
+    #atomic_df = PandasPdb().read_pdb(str(path)).df["ATOM"]
+    chain_pos_aa=compute_chain_pos_aa_mapping(pdb_df)
+    residue2node={}
+    node_index=0
+
+    for i in chain_pos_aa.keys():
+        my_chain=chain_pos_aa[i]
+        for item in my_chain:
+            residue2node[str(i)+str(item)]=str(node_index)
+            node_index+=1
+    return residue2node
+
+def nodes(pdb_id):
+    path='pdb_library/'+str(pdb_id)+'.pdb'
+    res2node=residueID2nodeID(pdb_id)
+    G = read_pdb(path)
+    sequence=['N']*G.number_of_nodes()
+    for node in G.nodes():
+
+        residueID=str(node[:-3])
+        aa_name=str(node[-3:])
+        nodeID=res2node[residueID]
+        sequence[int(nodeID)]=aa_name
+
+    return sequence
