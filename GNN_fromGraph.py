e87d4dac242343fd22327b7778fec1d56266995e
Gaoyuan Wang <gaoyuanwang@vpn172022112096.its.yale.internal>
2022-09-30 16:23:16 -0400

    .

--- GNN_fromGraph.py
+++ GNN_fromGraph.py
@@ -155,8 +155,7 @@ if __name__ == '__main__':
 
     label_test=[]
     for data in test_loader:  # Iterate in batches over the training/test dataset.
-        print(data.y)
-        label_test.append(data.y.item())
+        label_test.append(data.y.tolist())
     fpr1, tpr1, thresholds = roc_curve(label_test, predict_test)
     tn, fp, fn, tp = confusion_matrix(label_test, predict_test).ravel()
     AUROC = auc(fpr1, tpr1)
