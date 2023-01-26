11868ca1f6030a45f0495da1ba947cc5639fc125
brianhan-coder <82759867+brianhan-coder@users.noreply.github.com>
2023-01-25 23:31:33 -0500

    Update GNN_createGraph.py

--- GNN_createGraph.py
+++ GNN_createGraph.py
@@ -18,13 +18,14 @@ from multiprocessing import Pool
 import multiprocessing
 
 
-parser = argparse.ArgumentParser(description="create graph library for GNN training")
-parser.add_argument('-d','--dataset', required=True, help='the protein dataset')
+#构建命令行指令
+parser = argparse.ArgumentParser(description="create graph library for GNN training")#构建指令模块
+parser.add_argument('-d','--dataset', required=True, help='the protein dataset')#require必须输入，输入类型未指定
 parser.add_argument('--pdb_path', required=True, help='path to the pdb files')
 parser.add_argument('-g','--graph_path', required=True, help='path to the location where the graphs should be saved')
 parser.add_argument('-i','--type_input', required=True, help='specify whether it is AlphaFold input or PDB input')
-args = parser.parse_args()
-protein_dataset=args.dataset
+args = parser.parse_args()#传入参数
+protein_dataset=args.dataset#参数赋值
 pdb_path=args.pdb_path
 graph_path=args.graph_path
 type_input=args.type_input
@@ -64,22 +65,22 @@ featureData.readFeatureFile("AA_features/AA_PiSite.dat","PiSite")
 
 proteins=[]
 graph_labels=[]
-with open(protein_dataset, "r") as file:
+with open(protein_dataset, "r") as file: #r指定读文件，with as结构防止返回错误，建立一个临时指令空间
     content = file.read()
-for line in content.splitlines():
-    line=np.array(list(line.split("\t")))
-    proteins.append(line[0])
-    graph_labels.append(int(line[1]))
+for line in content.splitlines():#取文件中的行
+    line=np.array(list(line.split("\t")))#将行内信息以空格隔开再整入列表，之后构建array
+    proteins.append(line[0])#将array中的第0项存入proteins
+    graph_labels.append(int(line[1]))#将array中的第1项作为labels
 
-tmp = list(zip(proteins, graph_labels))
-random.shuffle(tmp)
-proteins, graph_labels = zip(*tmp)
-proteins, graph_labels = list(proteins), list(graph_labels)
+tmp = list(zip(proteins, graph_labels))#compress proteins and graph_labels，构成一一对应的列表
+random.shuffle(tmp)#随机化列表
+proteins, graph_labels = zip(*tmp)#展开zip
+proteins, graph_labels = list(proteins), list(graph_labels)#将array转回list
 
 
 if __name__ == '__main__':
     ### parallel converting PDB to graphs
-    for protein_index,my_protein in enumerate(proteins):
+    for protein_index,my_protein in enumerate(proteins):#enumerate()将一个可遍历iterable数据对象组合为一个索引序列，同时列出数据和数据下标。
         input_list=[pdb_path,my_protein,featureData,graph_labels,protein_index,type_input]
 
         G=GNN_core.convert_pdb2graph(input_list)
